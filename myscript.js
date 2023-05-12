24fd37627c0bfd2788c2ae4a13eabc2a9d895c8a
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-12 14:20:39 +0200

    comments

--- myscript.js
+++ myscript.js
@@ -9,8 +9,8 @@ const favoriteColor = prompt('Insert your favorite color');
 const currentYear = 23;
 
 // * visualizzo nell'html la password generata
-
-const generatedPw = document.getElementById('generated_password').innerHTML = userName + userLastName + favoriteColor + currentYear;
+// Il numero currentYear viene visualizzato come stringa, se volessimo fare un'operazione tra due variabili con valore numerico in una stringa usiamo le parentesi es. (currentYear + 34) = nomecognomecolorepreferito57
+const generatedPw = document.getElementById('generated_password').innerHTML = userName + userLastName + favoriteColor + currentYear; 
 
 // visualizzo messaggio di warning e password generata nell'alert
 
